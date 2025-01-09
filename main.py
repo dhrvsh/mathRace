e3a568bba15e3037d90fb1c54f480ab40a6a83b7
Dhruv Sharma <techyguy10@outlook.com>
2025-01-09 11:39:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,41 @@ import customtkinter
 from pyfonts import load_font
 
 font = load_font(
-    font_url=r"D:\MajorProjects\mathRace\mathRace\Bricolage_Grotesque\BricolageGrotesque.ttf"
+    font_url=r"https://github.com/dhrvsh/mathRace/blob/main/Bricolage_Grotesque/BricolageGrotesque.ttf?raw=true"
 )
 inputScreen = customtkinter.CTk()
 inputScreen.geometry("600x350")
 inputScreen.resizable(False, False)
 inputScreen.title("Project Castor")
 
-title = customtkinter.CTkLabel(inputScreen, text="Project Castor", fg_color="transparent", font=('Cartograph CF', 40)).place(x=200, y=10)
+title = customtkinter.CTkLabel(inputScreen, text="Project Castor", fg_color="transparent", font=('Cartograph CF', 35)).place(x=150, y=10)
 
 # label = customtkinter.CTkLabel(inputScreen, text="CTkLabel", fg_color="transparent", font=("Cascadia Mono", 40)).place(x=0, y=0)
 
-x1entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter first X", width=250)
-x1entry.place(x=0, y=75)
+xx = 270
+
+firstlabel = customtkinter.CTkLabel(inputScreen, text="First X and Y values: ", fg_color="transparent", font=('Cartograph CF', 15)).place(x=20, y=85)
+secondlabel = customtkinter.CTkLabel(inputScreen, text="Second X and Y values: ", fg_color="transparent", font=('Cartograph CF', 15)).place(x=20, y=135)
+thirdlabel = customtkinter.CTkLabel(inputScreen, text="Third X and Y values: ", fg_color="transparent", font=('Cartograph CF', 15)).place(x=20, y=185)
+
+x1entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter first X", width=150)
+x1entry.place(x=xx, y=85)
+
+x2entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter second X", width=150)
+x2entry.place(x=xx, y=135)
+
+x3entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter third X", width=150)
+x3entry.place(x=xx, y=185)
+
+
+y1entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter first Y", width=150)
+y1entry.place(x=xx+160, y=85)
+
+y2entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter second Y", width=150)
+y2entry.place(x=xx+160, y=135)
+
+y3entry = customtkinter.CTkEntry(inputScreen, placeholder_text="Enter third Y", width=150)
+y3entry.place(x=xx+160, y=185)
 
 inputScreen.mainloop()
 
